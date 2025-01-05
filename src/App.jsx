4741b5a0ed2170807a92d6edfb7754641d996343
ROHIN PRAMOD <rohinpramod320@gmail.com>
2025-01-05 16:43:07 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DigitalClock from './DigitalClock'
+
+
+const App = () => {
+  return (
+    <div>
+
+        <DigitalClock />
+    </div>
+  )
+}
+
+export default App
